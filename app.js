9ac28bd8575c20d0de306f9c80a7693bb28ac4e7
Ruben <ruben.astudillo@gmail.com>
2018-12-11 08:25:40 +0100

    Limpieza de comentarios

--- app.js
+++ app.js
@@ -6,4 +6,34 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direccion);
+const colors = require('colors');
+const lugar = require('./lugar/lugar');
+const clima = require('./clima/clima');
+
+
+// lugar.getLugarLatLng(argv.direccion)
+//     .then(resp => {
+//         clima.getClima(resp.latitud,resp.longitud);
+//     })
+//     .catch(e => console.log('Error: '.red, e))
+
+// clima.getClima('48.856614', '2.3522219')
+//     .then(temp => console.log(temp))
+//     .catch(e => console.log('Error:'.red, e))
+
+
+let getInformacion = async(direccion) => {
+
+    try {
+        let localiza = await lugar.getLugarLatLng(direccion);
+        let tiempo = await clima.getClima(localiza.latitud, localiza.longitud);
+        return `La temperatura de ${localiza.direccion} es de ${tiempo.temperatura} grados`;
+
+    } catch (error) {
+        console.log('Error'.red, error)
+    }
+}
+
+getInformacion(argv.direccion)
+    .then(respuesta => console.log(respuesta))
+    .catch(e => console.log('Error'.red, e))
